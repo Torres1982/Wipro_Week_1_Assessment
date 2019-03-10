68a210d763b74e685395fab824089d0c6371f482
Arturro <Torres1982@users.noreply.github.com>
2019-03-10 01:30:14 +0000

    Updated main.js file.
    [1] Reduced comments, [2] Added email validation, [3] Method that checks the radio button optimised (if statements replaced with a ternary operator).

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,4 @@
-/*********************************************************************/
-/*                  Validation of HTML input fields                  */
-/*********************************************************************/
+// Validation of HTML input fields
 function validateFields() {
 	var first_name = document.getElementById("first-name").value;
 	var surname = document.getElementById("surname").value;
@@ -8,44 +6,38 @@ function validateFields() {
     var dob = document.getElementById("date-of-birth").value;
 	var gender = $("input[name='gender']:checked");
     var gender_selected = [];
-    
+
 	if (first_name.length < 2) {
 		alert("Provide your First Name, please!");
 	} else if (surname.length < 2) {
 		alert("Provide your Surname, please!");
-	}  else if (email == "") {
-		alert("Provide Email, please!");
+	}  else if (!validateEmail(email)) {
+		alert("Provide Correct Format Email, please!");
 	} else if (gender.length === 0) {
 		alert("Select Gender, please!");
 	} else if (dob == "") {
         alert("Select Date of Birth, please!");
     } else {
 		confirm("Are you sure you want to submit your data?");
-        
+
         // Find the value of selected gender check box
-        $.each($("input[name='gender']:checked"), function(){            
+        $.each($("input[name='gender']:checked"), function(){
             gender_selected .push($(this).val());
         });
-        
+
         displayJsonArray(first_name, surname, email, gender_selected, dob);
         saveDataToLocalStorage(first_name, surname, email, gender_selected, dob);
-        //console.log("Text from the validation!");
 	}
 }
 
-/*********************************************************************/
-/*          This function returns a String of the user details       */
-/*********************************************************************/
+// This function returns a String of the user details
 function displayJsonArray (first_name, surname, email, gender, dob) {
     var user_details = '{"new_user":[{"first_name": ' + first_name + ', "last_name": ' + surname + ', "email": ' + email + ', "gender": ' + gender +', "date_of_birth": ' + dob + '}]}';
-    
+
     console.log(user_details);
-    //console.log("Text from the function!");
 }
 
-/*********************************************************************/
-/*          Saves the registered user in the local storage           */
-/*********************************************************************/
+// Saves the registered user in the local storage
 function saveDataToLocalStorage(first_name, last_name, email, gender, dob) {
     localStorage.setItem("first_name", first_name);
     localStorage.setItem("last_name", last_name);
@@ -54,55 +46,48 @@ function saveDataToLocalStorage(first_name, last_name, email, gender, dob) {
     localStorage.setItem("date_of_birth", dob);
 }
 
-/*********************************************************************/
-/*           Retrieves the registered user in the local storage      */
-/*********************************************************************/
+// Retrieves the registered user from the local storage
 $('.nav_users').click(function() {
     retrieveDataFromLocalStorage();
 });
 
 function retrieveDataFromLocalStorage() {
-    var name = localStorage.getItem("first_name");
+    var first_name = localStorage.getItem("first_name");
     var surname = localStorage.getItem("last_name");
     var email = localStorage.getItem("email");
     var gender = localStorage.getItem("gender");
     var dob = localStorage.getItem("date_of_birth");
-    var user_array = [name, surname, email, gender, dob];
-  
+    var user_array = [first_name, surname, email, gender, dob];
+
     populateInputFields(user_array);
 }
 
-/*********************************************************************/
-/*          Populate the input fields with data from local storage   */
-/*********************************************************************/
+// Populate the input fields with data from the local storage
 function populateInputFields(user_array) {
     $("#first-name").val(user_array[0]);
     $("#surname").val(user_array[1]);
     $("#email").val(user_array[2]);
     $("#date-of-birth").val(user_array[4]);
-    
+
     checkGenderCheckBox(user_array);
-    //alert("USER DETAILS: " + user_array);
 }
 
-/*********************************************************************/
-/*   Select the gender checkbox when auto-populating input fields    */
-/*********************************************************************/
+// Select the gender checkbox when auto-populating input fields
 function checkGenderCheckBox(user_array) {
-    if (user_array[3] === "female") {
-        $('#female').prop("checked", true);
-        //alert("FEMALE");
-    } else if (user_array[3] === "male") {
-        $('#male').prop("checked", true);
-        //alert("MALE");
-    }
+    user_array[3] === "female" ? $('#female').prop("checked", true) : $('#male').prop("checked", true);
 }
 
-/*********************************************************************/
-/*                    Toggle for the mobile menu button              */
-/*********************************************************************/
+// Simple email validation
+function validateEmail(email) {
+	var email_regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+	return email.match(email_regex);
+}
+
+// Toggle for the mobile menu button
 $('.mobile-menu-icon').click(function() {
 	var navigation = $('.main-nav');
-	
+
 	navigation.slideToggle(250);
 });
+
