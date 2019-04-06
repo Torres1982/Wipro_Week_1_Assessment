cdde982f82daa7856ed2ccbe34121d1851024ca7
Arturro <Torres1982@users.noreply.github.com>
2019-04-06 21:17:30 +0100

    [1] gender_selected array replaced with a string var. [2] Ternary operator broken into smaller parts.
    [3] jQuery each looping function replaced with a simple JS function to get selected radio button value. [4] Replace passed array into a function with passed variable (radio button). [5] Added more meaningful method name for adding user data to a local sorage.

--- src/js/main.js
+++ src/js/main.js
@@ -1,49 +1,47 @@
 // Validation of HTML input fields
 function validateFields() {
-	var first_name = document.getElementById("first-name").value;
-	var surname = document.getElementById("surname").value;
-	var email = document.getElementById("email").value;
+    var first_name = document.getElementById("first-name").value;
+    var surname = document.getElementById("surname").value;
+    var email = document.getElementById("email").value;
     var dob = document.getElementById("date-of-birth").value;
-	var gender = $("input[name='gender']:checked");
-    var gender_selected = [];
+    var gender = $("input[name='gender']:checked");
+    var gender_selected = gender.val();
 
-	if (first_name.length < 2) {
-		alert("Provide your First Name, please!");
-	} else if (surname.length < 2) {
-		alert("Provide your Surname, please!");
-	}  else if (!validateEmail(email)) {
-		alert("Provide Correct Format Email, please!");
-	} else if (gender.length === 0) {
-		alert("Select Gender, please!");
-	} else if (dob == "") {
+    if (first_name.length < 2) {
+	alert("Provide your First Name, please!");
+    } else if (surname.length < 2) {
+	alert("Provide your Surname, please!");
+    }  else if (!validateEmail(email)) {
+	alert("Provide Correct Format Email, please!");
+    } else if (gender.length === 0) {
+	alert("Select Gender, please!");
+    } else if (dob == "") {
         alert("Select Date of Birth, please!");
     } else {
-		confirm("Are you sure you want to submit your data?");
+	confirm("Are you sure you want to submit your data?");
 
-        // Find the value of selected gender check box
-        $.each($("input[name='gender']:checked"), function(){
-            gender_selected .push($(this).val());
-        });
-
-        displayJsonArray(first_name, surname, email, gender_selected, dob);
-        saveDataToLocalStorage(first_name, surname, email, gender_selected, dob);
-	}
+    displayJsonArray(first_name, surname, email, gender_selected, dob);
+    saveDataToLocalStorage(first_name, surname, email, gender_selected, dob);
+}
 }
 
 // This function returns a String of the user details
 function displayJsonArray (first_name, surname, email, gender, dob) {
     var user_details = '{"new_user":[{"first_name": ' + first_name + ', "last_name": ' + surname + ', "email": ' + email + ', "gender": ' + gender +', "date_of_birth": ' + dob + '}]}';
-
     console.log(user_details);
 }
 
 // Saves the registered user in the local storage
 function saveDataToLocalStorage(first_name, last_name, email, gender, dob) {
-    localStorage.setItem("first_name", first_name);
-    localStorage.setItem("last_name", last_name);
-    localStorage.setItem("email", email);
-    localStorage.setItem("gender", gender);
-    localStorage.setItem("date_of_birth", dob);
+    addUserDataToLocalDatabase("first_name", first_name);
+    addUserDataToLocalDatabase("last_name", last_name);
+    addUserDataToLocalDatabase("email", email);
+    addUserDataToLocalDatabase("gender", gender);
+    addUserDataToLocalDatabase("date_of_birth", dob);
+}
+
+function addUserDataToLocalDatabase(key, value) {
+    localStorage.setItem(key, value);
 }
 
 // Retrieves the registered user from the local storage
@@ -68,26 +66,24 @@ function populateInputFields(user_array) {
     $("#surname").val(user_array[1]);
     $("#email").val(user_array[2]);
     $("#date-of-birth").val(user_array[4]);
+    var gender = user_array[3] === "female" ? "female" : "male";
 
-    checkGenderCheckBox(user_array);
+    checkGenderCheckBox(gender);
 }
 
-// Select the gender checkbox when auto-populating input fields
-function checkGenderCheckBox(user_array) {
-    user_array[3] === "female" ? $('#female').prop("checked", true) : $('#male').prop("checked", true);
+function checkGenderCheckBox(gender) {
+    $('#'+gender).prop("checked", true);
 }
 
 // Simple email validation
 function validateEmail(email) {
-	var email_regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+     var email_regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-	return email.match(email_regex);
+     return email.match(email_regex);
 }
 
 // Toggle for the mobile menu button
 $('.mobile-menu-icon').click(function() {
-	var navigation = $('.main-nav');
-
-	navigation.slideToggle(250);
+     var navigation = $('.main-nav');
+     navigation.slideToggle(250);
 });
-
